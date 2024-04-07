bfec322ba031a6b15b8109e6d84a46da063bd486
karl18151 <aptx4568@gmail.com>
2024-04-07 16:36:05 +0800

    Site updated: 2024-04-07 16:36:04

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,12 +3,10 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
          document.title = '�������ߣ�';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
          document.title = '��˧�ͻ�����' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
