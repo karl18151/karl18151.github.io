2639997f3a57b9a28c407c0580feeff4543014f0
karl18151 <aptx4568@gmail.com>
2024-04-07 16:52:24 +0800

    Site updated: 2024-04-07 16:52:23

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,11 +3,11 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         document.title = '你丑你就走！';
+         document.title = '他转身就走了 像下定了某种决心';
          clearTimeout(titleTime);
      }
      else {
-         document.title = '你帅就回来！' + OriginTitle;
+         document.title = '靓仔，你终于回来了！！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
